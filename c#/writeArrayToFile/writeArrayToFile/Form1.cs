abb9894e8e56136cc953e4a4521ef8ff9aad2a3c
Greg Dunne <16168853+gregd89@users.noreply.github.com>
2024-05-25 14:03:41 +0100

    Add files via upload

---
+++ c#/writeArrayToFile/writeArrayToFile/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace writeArrayToFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Declare Array
+                int[] numbers = { 10, 20, 30, 40 };
+
+                StreamWriter outputfile;
+
+                outputfile = File.CreateText(@"C:\Users\GregDunne\Documents\College\2024\Principles of Programming\C# Programs\Files\values.txt");
+
+                for (int index = 0; index < numbers.Length; index++)
+                {
+                    outputfile.WriteLine(numbers[index]);
+                }
+                outputfile.Close();
+
+                MessageBox.Show("All Data Writing Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
